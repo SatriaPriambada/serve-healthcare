55755d58a4be661056d381db12b62cff0b8cedbe
Satria Priambada <spriambada3@eris.cc.gatech.edu>
2020-01-16 20:20:35 -0500

    try to reduce client error, in progress on eris

--- ensemble_profiler/patient_client.go
+++ ensemble_profiler/patient_client.go
@@ -8,29 +8,32 @@ import (
 	"time"
 	"math"
 	"math/rand"
+	"strconv"
 )
 
-var Exp_backoff_counter = 0
 var Maximum_backoff = 32000 //in millisecond
 
-func on_disconnect(url string, ch chan<- string){
-	Exp_backoff_counter++
-	const_backoff := math.Pow(2, float64(Exp_backoff_counter))
-	random_number_milliseconds := rand.Intn(Maximum_backoff)
-	final_backoff := math.Min((const_backoff+float64(random_number_milliseconds)), float64(Maximum_backoff))
-	fmt.Printf("backing off : %.2f ", final_backoff)
+func on_disconnect(url string, backoff_counter int, ch chan<- string){
+	backoff_counter++
+	increasing_backoff := math.Pow(2, float64(backoff_counter))
+	random_number_milliseconds := rand.Intn(1000)
+	final_backoff := math.Min((increasing_backoff+float64(random_number_milliseconds)), float64(Maximum_backoff))
+	fmt.Printf("backing off for the %d time: %.2f ", backoff_counter, final_backoff)
 	time.Sleep(time.Duration(final_backoff) * time.Millisecond)
-	go MakeRequest(url, ch)
+	// fmt.Println("finish sleeping : %.2f, now call request %s", final_backoff, url)
+	MakeRequest(url, backoff_counter, ch)
 }
-	
-func MakeRequest(url string, ch chan<- string) {
+
+func MakeRequest(url string, backoff_counter int, ch chan<- string) {
+	// fmt.Println("start request ", url)
+	http.DefaultClient.Timeout = time.Minute * 2
 	start := time.Now()
 	resp, err := http.Get(url)
 	secs := time.Since(start).Seconds()
 	if err != nil {
 		fmt.Println("handle error get")
 		fmt.Println("on_disconnect: ", err)
-		on_disconnect(url, ch)
+		on_disconnect(url, backoff_counter, ch)
 	}
 	defer resp.Body.Close()
 	body, errRead := ioutil.ReadAll(resp.Body)
@@ -40,25 +43,29 @@ func MakeRequest(url string, ch chan<- string) {
 	}
 	ch <- fmt.Sprintf("%.2f elapsed with response length: %s %s", secs, body, url)
 }
+
 func main() {
+	totalRequest:=3750
 	patientId:= flag.String("patientId", "0", "string to represent a patient client id")
 	flag.Parse()
 	fmt.Println("patient id:", *patientId)
 	start := time.Now()
 	ch := make(chan string)
-	for i := 0; i <= 3800; i++ {
+	for i := 0; i <= totalRequest; i++ {
 		// wait for 8 milliseconds to simulate the patient
 		// incoming data
 		time.Sleep(8 * time.Millisecond)
 		// This how actual client will send the result
 		// go MakeRequest("http://127.0.0.1:5000/hospital?patient_name=Adam&value=0.0&vtype=ECG", ch)
 		// This is how profiling result is send
-		fmt.Printf("client alive %s : loop: %d ", *patientId, i)
+		// fmt.Printf("client %s alive : loop: %d ", *patientId, i)
 		hostAddr := "http://127.0.0.1:5000"
-		go MakeRequest( hostAddr + "/hospital?patient_id=0" +"&value=0.0&vtype=ECG", ch)
+		go MakeRequest( hostAddr + "/hospital?patient_id=0" +"&value="+ strconv.Itoa(i) + ".0&vtype=ECG", 0, ch)
 	}
-	for i := 0; i <= 3800; i++ {
+	for i := 0; i <= totalRequest; i++ {
 		fmt.Println(<-ch)
 	}
-	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+	fmt.Printf("client finished %.2fs elapsed\n", time.Since(start).Seconds())
+	// sleep 1 minute to make sure all previous request and socket file descriptor are closed
+	time.Sleep(time.Minute * 1)
 }

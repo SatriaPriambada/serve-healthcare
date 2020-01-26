cd1bc1265c6782324f3f6ce11875b969e5c0abbb
Shenda Hong <hongshenda@pku.edu.cn>
2020-01-25 19:18:31 -0500

    finish solve_opt

--- exp_autoscale.py
+++ exp_autoscale.py
@@ -34,7 +34,7 @@ def get_description(n_gpu, n_patients):
 
     return V, c
 
-def random_sample(n_model, B, n_samples=1000):
+def random_sample(n_model, B, n_samples):
     """
     Input:
         n_model: number of models, n
@@ -58,6 +58,29 @@ def random_sample(n_model, B, n_samples=1000):
         i += 1
     return out
 
+def nearby_sample(n_model, B_top, B, n_samples, dist=3):
+    """
+    """
+    out = []
+    i = 0
+    while i < n_samples:
+        # get a random b from B_top
+        tmp = B_top[np.random.choice(list(range(len(B_top))))]
+        # random binary vector near dist, by random filp 3 digits
+        for j in range(dist):
+            idx = np.random.choice(list(range(n_model)))
+            if tmp[idx] == 0:
+                tmp[idx] = 1
+            else:
+                tmp[idx] = 0
+        # dedup
+        for b in B:
+            if np.array_equal(tmp, b):
+                break
+        out.append(tmp)
+        i += 1
+    return out
+
 def get_obj(accuracy, latency, lamda, L):
     return accuracy + lamda * (L - latency)
 
@@ -68,7 +91,7 @@ def save_checkpoint(res):
 ##################################################################################################
 # naive
 ##################################################################################################
-def solve_random(V, c, L):
+def solve_random(V, c, L, lamda):
     """
     random incremental
     """
@@ -88,7 +111,7 @@ def solve_random(V, c, L):
     print('found best b is: {}'.format(b))
     return b
 
-def solve_greedy_accuracy(V, c, L):
+def solve_greedy_accuracy(V, c, L, lamda):
     """
     greedy accuracy incremental
     """
@@ -107,7 +130,7 @@ def solve_greedy_accuracy(V, c, L):
     print('found best b is: {}'.format(b))
     return b
 
-def solve_greedy_latency(V, c, L):
+def solve_greedy_latency(V, c, L, lamda):
     """
     greedy latency incremental
     """
@@ -127,12 +150,105 @@ def solve_greedy_latency(V, c, L):
     return b
 
 ##################################################################################################
-# proxy
+# opt
 ##################################################################################################
-def solve_proxy(V, c):
+def solve_opt_passive(V, c, L, lamda):
     # --------------------- hyper parameters ---------------------
     
-    lamda = 10
+    N1 = 100 # warm start
+
+    # --------------------- initialization ---------------------
+    n_model = V.shape[0]
+    B = []
+    Y_accuracy = []
+    all_latency = []
+    Y_latency = []
+    res = {'B':B, 'Y_accuracy':Y_accuracy, 'Y_latency':Y_latency, 'all_latency':all_latency}
+
+    # --------------------- (1) warm start ---------------------
+    B = random_sample(n_model=n_model, B=B, n_samples=N1)
+    # profile
+    for b in tqdm(B):
+        Y_accuracy.append(get_accuracy_profile(V, b))
+        tmp_latency = get_latency_profile(V, c, b)
+        all_latency.append(tmp_latency)
+        Y_latency.append(np.percentile(tmp_latency, 95))
+        save_checkpoint(res)
+
+    # --------------------- (3) solve ---------------------
+    all_obj = []
+    for i in range(len(B)):
+        all_obj.append(get_obj(Y_accuracy[i], Y_latency[i], lamda, L))
+    opt_idx = np.argmax(all_obj)
+    opt_b = B[opt_idx]
+    print('found best b is: {}'.format(opt_b))
+    return opt_b
+
+def solve_opt_active(V, c, L, lamda):
+    # --------------------- hyper parameters ---------------------
+    
+    N1 = 10 # warm start
+    topK = 3 # search near top
+    N3 = 10 # profile
+    epoches = 10
+
+    # --------------------- initialization ---------------------
+    n_model = V.shape[0]
+    B = []
+    Y_accuracy = []
+    all_latency = []
+    Y_latency = []
+    res = {'B':B, 'Y_accuracy':Y_accuracy, 'Y_latency':Y_latency, 'all_latency':all_latency}
+
+    # --------------------- (1) warm start ---------------------
+    B = random_sample(n_model=n_model, B=B, n_samples=N1)
+    # profile
+    all_obj = []
+    for b in tqdm(B):
+        Y_accuracy.append(get_accuracy_profile(V, b))
+        tmp_latency = get_latency_profile(V, c, b)
+        all_latency.append(tmp_latency)
+        Y_latency.append(np.percentile(tmp_latency, 95))
+        save_checkpoint(res)
+        all_obj.append(get_obj(Y_accuracy[-1], Y_latency[-1], lamda, L))
+
+    # --------------------- (2) choose B ---------------------
+    for i_epoches in tqdm(range(epoches)):
+
+        # search 
+        top_idx = np.argsort(all_obj)[::-1][:topK]
+        B_top = list(np.array(B)[top_idx])
+        B_0 = nearby_sample(n_model, B_top, B, n_samples=N3)
+
+        # profile
+        for b in B_0:
+            # get_accuracy_profile
+            Y_accuracy.append(get_accuracy_profile(V, b))
+            # get_latency_profile
+            tmp_latency = get_latency_profile(V, c, b)
+            all_latency.append(tmp_latency)
+            Y_latency.append(np.percentile(tmp_latency, 95))
+            save_checkpoint(res)
+            all_obj.append(get_obj(Y_accuracy[-1], Y_latency[-1], lamda, L))
+
+        B = B + B_0
+        print(np.array(B).shape)
+
+    # --------------------- (3) solve ---------------------
+    all_obj = []
+    for i in range(len(B)):
+        all_obj.append(get_obj(Y_accuracy[i], Y_latency[i], lamda, L))
+    opt_idx = np.argmax(all_obj)
+    opt_b = B[opt_idx]
+    print('found best b is: {}'.format(opt_b))
+    return opt_b
+
+##################################################################################################
+# proxy
+##################################################################################################
+def solve_proxy(V, c, L, lamda):
+    # --------------------- hyper parameters ---------------------
+    
     N1 = 10 # warm start
     N2 = 1000 # proxy
     N3 = 10 # profile
@@ -206,11 +322,14 @@ def solve_proxy(V, c):
 if __name__ == "__main__":
     
     L = 0.1 # maximum latency
+    lamda = 10
     V, c = get_description(n_gpu=4, n_patients=1)
 
-    # solve_proxy(V, c, L)
-    # solve_random(V, c, L)
-    # solve_greedy_accuracy(V, c, L)
-    solve_greedy_latency(V, c, L)
+    # solve_random(V, c, L, lamda)
+    # solve_greedy_accuracy(V, c, L, lamda)
+    # solve_greedy_latency(V, c, L, lamda)
 
+    # solve_opt_passive(V, c, L, lamda)
+    solve_opt_active(V, c, L, lamda)
 
+    # solve_proxy(V, c, L, lamda)
